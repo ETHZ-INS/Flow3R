abbc4cec3774adad040f99620024fa27c3975a08
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-05-19 14:53:01 +0200

    fixup! Changing duration mid recording

---
+++ build_executable.bat
@@ -0,0 +1,11 @@
+:: The pyinstaller command is provided by the pyinstaller Python module, so the command needs to be executed inside the conda environment
+call conda activate Flow3R
+pyinstaller^
+ --add-data "flow3r/app/res/flow3r.png;./flow3r/app/res"^
+ --add-data "flow3r/app/res/flow3r.ico;./flow3r/app/res"^
+ --add-data "flow3r/app/res/style.qss;./flow3r/app/res"^
+ --onedir^
+ --noconsole^
+ --name "Flow3Rb"^
+ --icon "flow3r/app/res/flow3r.ico"^
+ test_open_two_pyav_cameras.py -y
